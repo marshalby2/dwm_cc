3b5ae43827ac13e25741a817331e13da28195061
marshal <marshalby2@gmail.com>
2023-09-07 10:04:37 +0800

    :fire: remove code

--- set_vol.sh
+++ set_vol.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-#dwmdir=$(cd $(dirname $0); cd .. ;pwd)
-
 sink=$(pactl info | grep 'Default Sink' | awk '{print $3}')
 vol=$(pactl list sinks | grep $sink -A 7 | sed -n '8p' | awk '{printf int($5)}')
 mod=$((vol % 5))
